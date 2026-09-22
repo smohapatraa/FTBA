58064b04c196026f91d9b50e7bb7d986abeaf1a3
smohapatraa <32749009+smohapatraa@users.noreply.github.com>
2026-09-22 19:06:06 +0400

    Update ftba.py

--- ftba.py
+++ ftba.py
@@ -908,6 +908,8 @@ elif mode == "night":
 
     st.markdown('<div class="ritual-label" style="margin-top:24px;">🎧 Night Listening</div>', unsafe_allow_html=True)
     st.video("https://www.youtube.com/watch?v=v9AHBtbk-E0")
+  st.video("https://www.youtube.com/watch?v=0FR8PNcazKk")
+
     st.caption("Let this night sound carry you into stillness. Close your eyes. Breathe.")
 
 # ─────────────────────────────────────────────
